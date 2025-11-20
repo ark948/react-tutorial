3148d28b78d24060e9a17ae18540a4df97547e8e
ark948 <armankarimi948@gmail.com>
2025-11-20 14:15:37 +0330

    Very simple button

--- react-web-project/src/App.jsx
+++ react-web-project/src/App.jsx
@@ -1,9 +1,16 @@
 import './App.css'
 
+function MyButton() {
+  return (
+    <button>I'm a button</button>
+  )
+}
+
 function App() {
   return (
     <>
       <h1>Welcome to my app</h1>
+      <MyButton />
     </>
   )
 }
