438697bd454c72591c8ee7288c5dfe7aa97b378f
ark948 <armankarimi948@gmail.com>
2025-11-20 14:29:35 +0330

    A bit more complicated component

--- react-web-project/src/App.jsx
+++ react-web-project/src/App.jsx
@@ -1,5 +1,28 @@
 import './App.css'
 
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+};
+
+function Profile() {
+  return (
+    <>
+      <h1>{user.name}</h1>
+      <img 
+        className='avatar'
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
+    </>
+  )
+}
+
 function MyButton() {
   return (
     <button className='btn'>I'm a button</button>
@@ -10,6 +33,7 @@ function App() {
   return (
     <>
       <h1>Welcome to my app</h1>
+      <Profile />
       <MyButton />
     </>
   )
