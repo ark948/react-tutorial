5c83fbc6b7c58a369654b8bc67dfe42609c1a2c5
ark948 <armankarimi948@gmail.com>
2025-12-19 22:22:11 +0330

    i don't know why css is not working

--- react-web-project/src/App.jsx
+++ react-web-project/src/App.jsx
@@ -1,6 +1,15 @@
+function MyButton() {
+    return (
+        <button className="avatar">
+            I'm a button
+        </button>
+    )
+}
+
 export default function App() {
     return (
         <div>
+            <MyButton />
             <h1>Hello in React</h1>
         </div>
     )
