6feb8d57e2af97db6dc68703e3c6afc022b61529
ark948 <armankarimi948@gmail.com>
2025-12-19 22:36:16 +0330

    Props

--- react-web-project/src/App.jsx
+++ react-web-project/src/App.jsx
@@ -1,24 +1,27 @@
 let message = 'This is cool';
 import { useState } from "react";
 
-function MyButton() {
+function MyButton({ name }) {
     const [count, setCount] = useState(0);
     function handleClick() {
-        console.log("Button clicked");
         setCount(count + 1);
     }
     return (
-        <button className="avatar" onClick={handleClick}>
-            Clicked {count} times.
-        </button>
+        <div>
+            <h3>Button {name}.</h3>
+            <button className="avatar" onClick={handleClick}>
+                Clicked {count} times.
+            </button>
+        </div>
     )
 }
 
 export default function App() {
     return (
         <div>
-            <MyButton />
-            <h4>Message is: {message}</h4>
+            <MyButton name={"A"} />
+            <MyButton name={"B"} />
+            <h6>Message is: {message}</h6>
             <h1>Hello in React</h1>
         </div>
     )
