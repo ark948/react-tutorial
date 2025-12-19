430227c5223f427a596c26fd6f90f41fe16c0759
ark948 <armankarimi948@gmail.com>
2025-12-19 22:23:14 +0330

    Rendered a variable

--- react-web-project/src/App.jsx
+++ react-web-project/src/App.jsx
@@ -1,3 +1,5 @@
+let message = 'This is cool';
+
 function MyButton() {
     return (
         <button className="avatar">
@@ -10,6 +12,7 @@ export default function App() {
     return (
         <div>
             <MyButton />
+            <h4>Message is: {message}</h4>
             <h1>Hello in React</h1>
         </div>
     )
