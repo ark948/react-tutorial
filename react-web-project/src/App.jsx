ac0fe7907a7d6a6a19f6dab2584202bcd8eb9bc3
ark948 <armankarimi948@gmail.com>
2025-12-20 20:21:06 +0330

    Simple square (CSS also updated)

--- react-web-project/src/App.jsx
+++ react-web-project/src/App.jsx
@@ -1,28 +1,13 @@
-let message = 'This is cool';
 import { useState } from "react";
 
-function MyButton({ name }) {
-    const [count, setCount] = useState(0);
-    function handleClick() {
-        setCount(count + 1);
-    }
-    return (
-        <div>
-            <h3>Button {name}.</h3>
-            <button className="avatar" onClick={handleClick}>
-                Clicked {count} times.
-            </button>
-        </div>
-    )
+function Square() {
+    return <button className="square">X</button>
 }
 
 export default function App() {
     return (
-        <div>
-            <MyButton name={"A"} />
-            <MyButton name={"B"} />
-            <h6>Message is: {message}</h6>
-            <h1>Hello in React</h1>
-        </div>
+        <>
+            <Square />
+        </>
     )
 }
