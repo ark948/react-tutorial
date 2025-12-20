de52ee7084736984386023112a62d21a1ab9b2c6
ark948 <armankarimi948@gmail.com>
2025-12-20 20:30:24 +0330

    Updating structure

--- react-web-project/src/App.jsx
+++ react-web-project/src/App.jsx
@@ -1,13 +1,35 @@
 import { useState } from "react";
 
 function Square() {
-    return <button className="square">X</button>
+    return <button className="square">1</button>;
+}
+
+function Board() {
+    return (
+        <>
+            <div className="board-row">
+                <Square />
+                <Square />
+                <Square />
+            </div>
+            <div className="board-row">
+                <Square />
+                <Square />
+                <Square />
+            </div>
+            <div className="board-row">
+                <Square />
+                <Square />
+                <Square />
+            </div>
+        </>
+    )
 }
 
 export default function App() {
     return (
         <>
-            <Square />
+            <Board />
         </>
     )
 }
