eeadb5cd644d71e80b9a4b568d494f51c385db30
ark948 <armankarimi948@gmail.com>
2025-12-25 21:52:56 +0330

    mdn done

--- mdn-react-tutorial/src/App.jsx
+++ mdn-react-tutorial/src/App.jsx
@@ -1,19 +1,10 @@
 import './App.css'
 
-const subject = "React";
 function App(props) {
-  console.log(props); // {subject: 'Clarice'}
-  console.log(props.subject); // Clarice
   return (
     <>
       <header>
-        <h1>Hello, {subject}.</h1>
-        {/* Hello, React :)! How you doing?*/}
-        <h1>Hello, {`${subject} :)`}!</h1>
-        {/* Hello, REACT */}
-        <h1>Hello, {subject.toUpperCase()}</h1>
-        {/* Hello, 4! */}
-        <h1>Hello, {2 + 2}!</h1>
+        <h1>Hello, dear {props.subject}.</h1>
         <button type="button" className="primary">Click me!</button>
       </header>
     </>
