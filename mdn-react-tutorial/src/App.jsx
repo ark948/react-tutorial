6819cbb5bac7450ed3db8d31077b1346e6ee1da1
ark948 <armankarimi948@gmail.com>
2025-12-25 21:46:34 +0330

    Valid js inside curly braces in JSX

--- mdn-react-tutorial/src/App.jsx
+++ mdn-react-tutorial/src/App.jsx
@@ -1,11 +1,18 @@
 import './App.css'
 
+const subject = "React";
 function App() {
-
   return (
     <>
       <header>
-        <h1>Hello, World!</h1>
+        <h1>Hello, {subject}.</h1>
+        {/* Hello, React :)! */}
+        <h1>Hello, {`${subject} :)`}!</h1>
+        {/* Hello, REACT */}
+        <h1>Hello, {subject.toUpperCase()}</h1>
+        {/* Hello, 4! */}
+        <h1>Hello, {2 + 2}!</h1>
+        <button type="button" className="primary">Click me!</button>
       </header>
     </>
   )
